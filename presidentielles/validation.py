f9ebe234fc29be708731f55362321335d2f19ec3
Rémi Louf <remilouf@gmail.com>
2021-08-28 00:44:18 +0200

    wip - ajouter sondages presidentielle

---
+++ presidentielles/validation.py
@@ -0,0 +1,40 @@
+import json
+
+
+class Resultats(BaseModel):
+    intentions_exprimees: Optional[int]
+    intentions: Dict[str, float]
+    certitude: Dict[str, float]
+
+candidats = [
+    "Nathalie Arthaud",
+    "Philippe Poutou",
+    "Fabien Roussel",
+]
+
+instituts = ["Harris interactive"]
+
+compulsory_fields = ["premier_tour", "second_tour"]
+optional_fields = ["reports_2017", "reports_premier_tour"]
+fields = compulsory_fields + optional_fields
+
+
+if __name__ == "__main__":
+    # This will raise an error if the JSON is not properly formatted
+    with open('sondages.json', 'r') as data:
+        sondages = json.load(data)
+
+    # Verifie que le % total sur la base des suffrages exprimés est égale
+    # à 100
+    # Vérifier le nom des candidats / config
+    # Vérifier que le nom des hypothèses est cohérent à chaque fois
+    # - toutes les hypotheses se retrouvent dans les intentions de vote
+    # - les hypotheses des reports existent dans la liste d'hypotheses
+    # Verifier la taille des matrices de report
+    # - 2017 -> 2022
+    # - depuis le premier tour
+    # Verifier que valeurs au premier tour dans un range raisonnabele (<40)
+    # Verifier que toutes les cles sont la, warning if extra (force schema)
+    # vérifier date fin > date debut
+    # verifier publication > debut
+    # verifier range raisonnable (< today et > jan 2020)
